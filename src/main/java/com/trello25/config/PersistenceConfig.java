cb5a18eaee5475c9449bb7fdfc3c371e0206c71a
Choi1999 <dprtldk157@gmail.com>
2024-10-16 07:25:53 +0900

    init feature/jwt

---
+++ src/main/java/com/trello25/config/PersistenceConfig.java
@@ -0,0 +1,9 @@
+package com.trello25.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig {
+}

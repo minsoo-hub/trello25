3d092a9ffd21d5dad39635b0c024d89279ca59a6
Choi1999 <dprtldk157@gmail.com>
2024-10-16 07:41:16 +0900

    init feature/jwt

--- src/main/java/com/trello25/config/JwtFilter.java
+++ src/main/java/com/trello25/config/JwtFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 
@@ -17,6 +18,7 @@ import java.io.IOException;
 
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class JwtFilter implements Filter {
 

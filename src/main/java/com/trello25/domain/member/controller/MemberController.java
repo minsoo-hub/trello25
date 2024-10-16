88bbea495ef001d0d670bdc07cd92765e5a2d9eb
mj-song00 <niveus128@gmail.com>
2024-10-16 23:01:05 +0900

    feat: add member api

--- src/main/java/com/trello25/domain/member/controller/MemberController.java
+++ src/main/java/com/trello25/domain/member/controller/MemberController.java
@@ -1,7 +1,39 @@
 package com.trello25.domain.member.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.trello25.domain.auth.dto.AuthUser;
+import com.trello25.domain.member.dto.ChangePermissionRequest;
+import com.trello25.domain.member.entity.Member;
+import com.trello25.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
+    //member 추가
+    @PostMapping("/{workspaceId}/{userId}")
+    public ResponseEntity<Member> addMembers(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PathVariable Long workspaceId,
+            @PathVariable Long userId
+    ){
+        memberService.addMembers(authUser,workspaceId, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    //member permission 변경
+    @PutMapping("/{id}")
+    public ResponseEntity<Member> changePermission(
+            @AuthenticationPrincipal AuthUser authUser,
+            @PathVariable Long id,
+            @RequestBody ChangePermissionRequest request
+    ){
+        return memberService.changePermission(authUser, id, request);
+    }
 }

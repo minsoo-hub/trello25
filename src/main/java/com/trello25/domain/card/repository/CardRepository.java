80a5a58dd36f173654a4850b4b31584b7784c47a
Choi1999 <dprtldk157@gmail.com>
2024-10-16 11:54:35 +0900

    init fix/jwt

--- src/main/java/com/trello25/domain/card/repository/CardRepository.java
+++ src/main/java/com/trello25/domain/card/repository/CardRepository.java
@@ -1,8 +1,7 @@
 package com.trello25.domain.card.repository;
 
-import javax.smartcardio.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardRepository extends JpaRepository<Card, Long> {
+//public interface CardRepository extends JpaRepository<Card, Long> {
 
-}
+//}

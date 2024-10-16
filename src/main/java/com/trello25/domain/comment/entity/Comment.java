d45dd19425baa0069849a9eb447f63801a4c7f42
naminsu <naminsoo1020@naver.com>
2024-10-17 03:33:31 +0900

    feat: 카드의 댓글 관련 코드 추가

--- src/main/java/com/trello25/domain/comment/entity/Comment.java
+++ src/main/java/com/trello25/domain/comment/entity/Comment.java
@@ -1,10 +1,14 @@
 package com.trello25.domain.comment.entity;
 
+import com.trello25.domain.card.entity.Card;
 import com.trello25.domain.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,4 +21,13 @@ public class Comment extends BaseEntity {
     private Long id;
 
     private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id", nullable = false)
+    private Card card;
+
+    public Comment(String text,Card card) {
+        this.text = text;
+        this.card = card;
+    }
 }
